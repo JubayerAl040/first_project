7f0bcc3c77f160b831f905812ede14be6f730bf4
jubayer040 <135593620+JubayerAl040@users.noreply.github.com>
2024-02-11 18:39:55 +0600

    order screen added

--- lib/screens/inapp_update_page.dart
+++ lib/screens/inapp_update_page.dart
@@ -1,9 +1,10 @@
 import 'package:first_project/screens/book_lawyer_screen.dart';
 import 'package:first_project/screens/book_pet_travel_screen.dart';
 import 'package:first_project/screens/book_photograph_screen.dart';
+import 'package:first_project/screens/order_screen.dart';
 import 'package:first_project/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter/scheduler.dart';
 
 class InAppUpdateScreen extends StatefulWidget {
   const InAppUpdateScreen({super.key});
@@ -12,44 +13,19 @@ class InAppUpdateScreen extends StatefulWidget {
 }
 
 class _InAppUpdateScreenState extends State<InAppUpdateScreen> {
-  final List<int> _scheduleDates = [1, 3, 4, 6, 7];
-  final _dateController = TextEditingController();
-  final _clockController = TextEditingController();
-
-  bool _isDateHighlighted(DateTime date) {
-    for (int i = 0; i < _scheduleDates.length; i++) {
-      if (_scheduleDates[i] == date.weekday) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  void _checkHeighLightDate() async {
-    final currentDate = DateTime.now();
-    DateTime? date = await showDatePicker(
-      context: context,
-      initialDate: currentDate,
-      firstDate: currentDate,
-      lastDate: currentDate.add(const Duration(days: 365)),
-      selectableDayPredicate: _isDateHighlighted,
-    );
-    if (date == null) return;
-    // user can't take todays appointment soo um getting remainingHour
-    // extra -1, bcz tommorrows date works perfectly either accurate hour cz prb
-    final todaysRemainingHour = 24 - (currentDate.hour) - 1;
-    if (date.isBefore(currentDate.add(Duration(hours: todaysRemainingHour)))) {
-      Fluttertoast.showToast(msg: "Sorry! This date isn't available");
-      return;
-    }
-    print("Selected Date: $date");
-  }
-
   @override
   Widget build(BuildContext context) {
     return PopScope(
-      onPopInvoked: (_) async => await showExitPopup(context),
+      canPop: false,
+      onPopInvoked: (bool didPop) async {
+        if (didPop) {
+          print("true");
+          SchedulerBinding.instance.addPostFrameCallback((_) {
+            Navigator.pop(context);
+          });
+        }
+        showExitPopup(context);
+      },
       child: Scaffold(
         appBar: AppBar(title: const Text('In App Review Example')),
         body: Padding(
@@ -96,8 +72,11 @@ class _InAppUpdateScreenState extends State<InAppUpdateScreen> {
               ),
               const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: _checkHeighLightDate,
-                child: const Text('Check Heighlight Date'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const OrderScreen()));
+                },
+                child: const Text('Go To Order Screen'),
               ),
               const SizedBox(height: 20),
             ],
@@ -105,13 +84,6 @@ class _InAppUpdateScreenState extends State<InAppUpdateScreen> {
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _dateController.dispose();
-    _clockController.dispose();
-    super.dispose();
   }
 }
 
---
+++ lib/screens/order_screen.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: DefaultTabController(
+        length: 3,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, value) {
+            return [
+              const SliverAppBar(
+                backgroundColor: Colors.black,
+                centerTitle: true,
+                title: Text("Activity", style: TextStyle(color: Colors.white)),
+              ),
+              SliverAppBar(
+                shape: const ContinuousRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.circular(35)),
+                ),
+                backgroundColor: Colors.black,
+                pinned: true,
+                automaticallyImplyLeading: false,
+                elevation: 10,
+                title: TabBar(
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                  dividerColor: Colors.transparent,
+                  indicator: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(8)),
+                  ),
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  padding: const EdgeInsets.only(top: 20),
+                  tabs: [
+                    _getTabBar("History"),
+                    _getTabBar("InProgress"),
+                    _getTabBar("Favorites"),
+                  ],
+                ),
+              ),
+            ];
+          },
+          body: const TabBarView(children: [
+            OrderHistoryList(),
+            OrderHistoryList(),
+            OrderHistoryList(),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  Widget _getTabBar(String title) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 7),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
+      ),
+      child: Center(child: Text(title)),
+    );
+  }
+}
+
+class OrderHistoryList extends StatelessWidget {
+  const OrderHistoryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: CustomScrollView(
+        slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.all(10),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                childCount: 3,
+                (context, i) {
+                  return const OrderListItem();
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OrderListItem extends StatelessWidget {
+  const OrderListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 7),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey[400]!,
+            spreadRadius: 1,
+            blurRadius: 15,
+            offset: const Offset(5, 5),
+          ),
+          const BoxShadow(color: Colors.white, spreadRadius: 1, blurRadius: 30),
+        ],
+      ),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Online Retail Store",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "#DA865DA2679",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 11,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Luggage Spot",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 11,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Text(
+                      "Quote Requested 14th January 2024",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 11,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 10),
+              Column(
+                children: [
+                  _getStatusItem("Quote Sent", Colors.green),
+                  const SizedBox(height: 5),
+                  _getStatusItem("View Details", Colors.grey[200]!),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(height: 10),
+          _getTimeLocation(
+            Icons.calendar_month,
+            DateFormat.yMMMd().format(DateTime.now()),
+            "${TimeOfDay.now().format(context)}-${TimeOfDay(hour: TimeOfDay.now().hour + 2, minute: TimeOfDay.now().minute).format(context)}",
+          ),
+          _getTimeLocation(
+            Icons.location_on_outlined,
+            "218 Eastern Avenu, IG4 5AB",
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getStatusItem(String title, Color color) {
+    return DecoratedBox(
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(5), color: color),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 7),
+        child: Text(
+          title,
+          style: const TextStyle(fontSize: 12),
+        ),
+      ),
+    );
+  }
+
+  Widget _getTimeLocation(IconData icon, String title, [String? time]) {
+    return Row(
+      children: [
+        Icon(icon, size: 15),
+        const SizedBox(width: 10),
+        Text(title, style: const TextStyle(fontSize: 12)),
+        const SizedBox(width: 10),
+        time != null
+            ? _getStatusItem(time, Colors.redAccent)
+            : const SizedBox(),
+      ],
+    );
+  }
+}
